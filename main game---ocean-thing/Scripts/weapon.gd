648aa34e4cb1ff5891fd98370a0af3ffb91b91f8
siroverlarps-png <voidedkafugong@gmail.com>
2026-07-04 18:57:23 +1000

    f u github i hate planet earth gng
    switched harpoon and weapon inputs

--- main game---ocean-thing/Scripts/weapon.gd
+++ main game---ocean-thing/Scripts/weapon.gd
@@ -1,25 +1,54 @@
 extends AnimationPlayer
+
 @onready var animation_player: AnimationPlayer = $"."
-@onready var sprite_2d: Sprite2D = $"../Sprite2D"
-var direction = "right"
 @onready var weapon: Node2D = $".."
 
+var direction = "right"
+
+
 func _ready() -> void:
-	animation_player.play("RESET_right")
+	update_direction_from_mouse(true)
 
 
-func _process(delta):
-	#print(direction)
-	if Input.is_action_just_pressed("right_click") and direction == "right":
-		animation_player.play("slash_right")
-	elif Input.is_action_just_pressed("right_click") and direction == "left":
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("left_click"):
+		update_direction_from_mouse(false)
+
+		if direction == "right":
+			animation_player.play("slash_right")
+		else:
 			animation_player.play("slash_left")
-	if Input.is_action_just_pressed("Left"):
+
+		return
+
+	if not is_attacking():
+		update_direction_from_mouse(true)
+
+
+func update_direction_from_mouse(play_reset_animation: bool) -> void:
+	var mouse_position = weapon.get_global_mouse_position()
+
+	if mouse_position.x >= weapon.global_position.x:
+		direction = "right"
+		weapon.scale.x = abs(weapon.scale.x)
+
+		if play_reset_animation:
+			animation_player.play("RESET_right")
+	else:
 		direction = "left"
 		weapon.scale.x = -abs(weapon.scale.x)
-		animation_player.play("RESET_left")
-	if Input.is_action_just_pressed("Right"):
-		direction = "right"
-		weapon.scale = abs(weapon.scale)
-		animation_player.play("RESET_right")
-		
+
+		if play_reset_animation:
+			animation_player.play("RESET_left")
+
+
+func is_attacking() -> bool:
+	return animation_player.is_playing() and (
+		animation_player.current_animation == "slash_right"
+		or animation_player.current_animation == "slash_left"
+	)
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "slash_right" or anim_name == "slash_left":
+		update_direction_from_mouse(true)
